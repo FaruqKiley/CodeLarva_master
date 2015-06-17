2ac7491549ee788e4dcbfb049e4ef4e19bb50a33
Sandeep Vattapparambil <sandeepv68@gmail.com>
2015-06-17 13:29:27 +0530

    Side-nav scroll bar changed to nicescroll

--- js/nicescroll_customization.js
+++ js/nicescroll_customization.js
@@ -3,4 +3,10 @@ $(document).ready(function() {
 			cursorcolor:"#666",
 			cursorborder: "1px solid #666"
 			});
-    });
+});
+$(document).ready(function() {  
+        $(".side-nav").niceScroll({
+			cursorcolor:"#666",
+			cursorborder: "1px solid #666"
+			});
+});
