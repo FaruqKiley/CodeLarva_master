7fab493d4e197567098dd56ae6bbccb058442679
Sandeep Vattapparambil <sandeepv68@gmail.com>
2015-06-17 22:00:10 +0530

    Area chart added

--- chart/js.js
+++ chart/js.js
@@ -53,3 +53,17 @@
         }
     }
 });
+var chart3 = c3.generate({
+	bindto: '#chart3',
+    data: {
+        columns: [
+            ['data1', 300, 350, 300, 0, 0, 0],
+            ['data2', 130, 100, 140, 200, 150, 50]
+        ],
+        types: {
+            data1: 'area',
+            data2: 'area-spline'
+        }
+    }
+});
+
