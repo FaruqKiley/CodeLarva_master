c5b38bfe6b7b9c172a90c59ea10423fd138c7813
Sandeep Vattapparambil <sandeepv68@gmail.com>
2015-06-17 21:55:02 +0530

    step chart added

--- chart/js.js
+++ chart/js.js
@@ -39,3 +39,17 @@
 		}
 	  }
   });
+  
+  var chart2 = c3.generate({
+	 bindto: '#chart2',
+    data: {
+        columns: [
+            ['data1', 300, 350, 300, 0, 0, 100],
+            ['data2', 130, 100, 140, 200, 150, 50]
+        ],
+        types: {
+            data1: 'step',
+            data2: 'area-step'
+        }
+    }
+});
